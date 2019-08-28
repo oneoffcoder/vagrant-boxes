b7bd2b89098b193eaab4d881d0c429f0629b737e
Jee Vang, Ph.D <vangjee@gmail.com>
2019-08-28 18:45:38 -0400

    default yes

--- base/scripts/cleanup.sh
+++ base/scripts/cleanup.sh
@@ -2,7 +2,7 @@
 
 apt-get remove -y --purge libreoffice* thunderbird* rhythmbox* aisleriot gnome-sudoku gnome-mahjongg mahjongg ace-of-penguins gnomine gbrainy gnome-mines byobu xiterm+thai cheese remmina gnome-todo totem totem-plugins
 apt-get clean
-apt-get autoremove
+apt-get autoremove -y
 echo 'Hidden=true' | cat /usr/share/applications/ubuntu-amazon-default.desktop - > ~/.local/share/applications/ubuntu-amazon-default.desktop
 rm -fr /var/lib/apt/lists/* \
     /tmp/* \
