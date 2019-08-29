81ef3c6b7c041a588d78cc8be069f061d4a5fa68
Jee Vang, Ph.D <vangjee@gmail.com>
2019-08-29 18:09:56 -0400

    change order; don't do anything crazy with cleanup

--- base/scripts/cleanup.sh
+++ base/scripts/cleanup.sh
@@ -1,13 +1,5 @@
 #!/bin/sh -eux
 
-apt-get remove -y --purge libreoffice* thunderbird* rhythmbox* aisleriot gnome-sudoku gnome-mahjongg mahjongg ace-of-penguins gnomine gbrainy gnome-mines byobu xiterm+thai cheese remmina gnome-todo totem totem-plugins
-apt-get clean
-apt-get autoremove -y
+apt-get remove -y --purge libreoffice* thunderbird* rhythmbox* xiterm+thai
 mkdir -p /home/vagrant/.local/share/applications/
 echo 'Hidden=true' | cat /usr/share/applications/ubuntu-amazon-default.desktop - > /home/vagrant/.local/share/applications/ubuntu-amazon-default.desktop
-rm -fr /var/lib/apt/lists/* \
-    /tmp/* \
-    /var/tmp/* \
-    /var/cache/apt/archives/lock \
-    /var/lib/dpkg/lock* \
-    /home/vagrant/Downloads/*
