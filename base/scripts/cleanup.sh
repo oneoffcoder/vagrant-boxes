919dc2c70695360cbd32be54c8720b7e8aa6403c
Jee Vang, Ph.D <vangjee@gmail.com>
2019-08-29 19:44:22 -0400

    remove

--- base/scripts/cleanup.sh
+++ base/scripts/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
 
-apt-get remove -y --purge libreoffice* thunderbird* rhythmbox* xiterm+thai
+apt-get remove -y --purge libreoffice* thunderbird* rhythmbox* aisleriot gnome-sudoku gnome-mahjongg mahjongg ace-of-penguins gnomine gbrainy gnome-mines xiterm+thai cheese remmina gnome-todo totem totem-plugins
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
 dpkg -i /tmp/google-chrome-stable_current_amd64.deb
 rm -f /tmp/google-chrome-stable_current_amd64.deb
