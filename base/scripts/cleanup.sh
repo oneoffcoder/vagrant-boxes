ee663fa9d55deace7c5d36b00839151ff3ad7381
Jee Vang, Ph.D <vangjee@gmail.com>
2019-08-28 20:01:02 -0400

    remove

--- base/scripts/cleanup.sh
+++ base/scripts/cleanup.sh
@@ -3,11 +3,11 @@
 apt-get remove -y --purge libreoffice* thunderbird* rhythmbox* aisleriot gnome-sudoku gnome-mahjongg mahjongg ace-of-penguins gnomine gbrainy gnome-mines byobu xiterm+thai cheese remmina gnome-todo totem totem-plugins
 apt-get clean
 apt-get autoremove -y
-echo 'Hidden=true' | cat /usr/share/applications/ubuntu-amazon-default.desktop - > ~/.local/share/applications/ubuntu-amazon-default.desktop
+mkdir -p /home/vagrant/.local/share/applications/
+echo 'Hidden=true' | cat /usr/share/applications/ubuntu-amazon-default.desktop - > /home/vagrant/.local/share/applications/ubuntu-amazon-default.desktop
 rm -fr /var/lib/apt/lists/* \
     /tmp/* \
     /var/tmp/* \
     /var/cache/apt/archives/lock \
     /var/lib/dpkg/lock* \
     /home/vagrant/Downloads/*
-history -c
