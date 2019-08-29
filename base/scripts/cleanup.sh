57acb22d3d81227487bd38df9df952ad4dde8b86
Jee Vang, Ph.D <vangjee@gmail.com>
2019-08-29 19:42:19 -0400

    remove; if kept then ubuntu unity doesn't load

--- base/scripts/cleanup.sh
+++ base/scripts/cleanup.sh
@@ -1,9 +1,6 @@
 #!/bin/sh -eux
 
 apt-get remove -y --purge libreoffice* thunderbird* rhythmbox* xiterm+thai
-mkdir -p /home/vagrant/.local/share/applications/
-echo 'Hidden=true' | cat /usr/share/applications/ubuntu-amazon-default.desktop - > /home/vagrant/.local/share/applications/ubuntu-amazon-default.desktop
-
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
 dpkg -i /tmp/google-chrome-stable_current_amd64.deb
 rm -f /tmp/google-chrome-stable_current_amd64.deb
